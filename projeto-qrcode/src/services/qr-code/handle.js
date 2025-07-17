21f4b3b73d54062c0e60b826d36245c8b8b352cc
Davi Lobo <lobodavi@hotmail.com>
2025-07-17 15:15:44 -0300

    Update handle.js

--- projeto-qrcode/src/services/qr-code/handle.js
+++ projeto-qrcode/src/services/qr-code/handle.js
@@ -1,18 +1,15 @@
-import qr from "qrcode-terminal";
-import chalk from "chalk";
+import qr from "qrcode-terminal"
+import chalk from "chalk"
 
 async function handle(err, result) {
-  if (err) {
-    console.log("error on application");
-    return;
-  }
-
-  const isSmall = result.type == 2;
-
-  qr.generate(result.link, { small: isSmall }, (qrcode) => {
-    console.log(chalk.green("QR Code gerado com sucesso:\n"));
-    console.log(qrcode);
-  });
+    if(err){
+        console.log("error on application")
+        return
+    }
+    const isSmall = result.type == 2
+    qr.generate(result.link, {small: isSmall}, (qrcode)=>{
+        console.log(chalk.green("QR CODE gerado com sucesso."))
+        console.log(qrcode)
+    })
 }
-
-export default handle;
+export default handle
