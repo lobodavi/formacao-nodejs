99a279ba3deb18013a81e8542dfd07cdea90b63d
Davi Lobo <lobodavi@hotmail.com>
2025-07-17 15:14:18 -0300

    Update index.js

--- projeto-qrcode/src/index.js
+++ projeto-qrcode/src/index.js
@@ -1,19 +1,15 @@
-import prompt from "prompt";
-
-import promptSchemaMain from "./prompts-schema/prompt-schema-main.js";
-
+import prompt from "prompt"
+import mainPrompt from "./prompts-schema/prompt-main.js"
 import createQRCode from "./services/qr-code/create.js";
 import createPassword from "./services/password/create.js";
 
 async function main() {
-  prompt.get(promptSchemaMain, async (err, choose) => {
-    if (err) console.log(err);
-
-    if (choose.select == 1) await createQRCode();
-    if (choose.select == 2) await createPassword();
-  });
-
-  prompt.start();
+    prompt.get(mainPrompt, async (err,choose) => {
+        if(choose.select == 1) await createQRCode()
+        if(choose.select == 2) await createPassword()
+        } 
+    )
+    prompt.start();
 }
 
 main();
